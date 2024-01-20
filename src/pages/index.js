f29096563de47bb32e028df77e11dbc53c7fa109
Emmanuel Calle <emacalle@hotmail.com>
2024-01-20 19:00:50 +0100

    last try?

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,65 +1,66 @@
-import * as React from "react"
+import * as React from "react";
+import { useState } from "react";
 
 const pageStyles = {
   color: "#232129",
   padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+  fontFamily: "-apple-system, Roboto, sans-serif, serif"
+};
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
-  maxWidth: 320,
-}
+  maxWidth: 320
+};
 const headingAccentStyles = {
-  color: "#663399",
-}
+  color: "#663399"
+};
 const paragraphStyles = {
-  marginBottom: 48,
-}
+  marginBottom: 48
+};
 const codeStyles = {
   color: "#8A6534",
   padding: 4,
   backgroundColor: "#FFF4DB",
   fontSize: "1.25rem",
-  borderRadius: 4,
-}
+  borderRadius: 4
+};
 const listStyles = {
   marginBottom: 96,
-  paddingLeft: 0,
-}
+  paddingLeft: 0
+};
 const listItemStyles = {
   fontWeight: 300,
   fontSize: 24,
   maxWidth: 560,
-  marginBottom: 30,
-}
+  marginBottom: 30
+};
 
 const linkStyle = {
   color: "#8954A8",
   fontWeight: "bold",
   fontSize: 16,
-  verticalAlign: "5%",
-}
+  verticalAlign: "5%"
+};
 
 const docLinkStyle = {
   ...linkStyle,
   listStyleType: "none",
-  marginBottom: 24,
-}
+  marginBottom: 24
+};
 
 const descriptionStyle = {
   color: "#232129",
   fontSize: 14,
   marginTop: 10,
   marginBottom: 0,
-  lineHeight: 1.25,
-}
+  lineHeight: 1.25
+};
 
 const docLink = {
   text: "Documentation",
   url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
+  color: "#8954A8"
+};
 
 const badgeStyle = {
   color: "#fff",
@@ -74,8 +75,8 @@ const badgeStyle = {
   position: "relative",
   top: -2,
   marginLeft: 10,
-  lineHeight: 1,
-}
+  lineHeight: 1
+};
 
 const links = [
   {
@@ -83,35 +84,35 @@ const links = [
     url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
     description:
       "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
+    color: "#E95800"
   },
   {
     text: "How to Guides",
     url: "https://www.gatsbyjs.com/docs/how-to/",
     description:
       "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
+    color: "#1099A8"
   },
   {
     text: "Reference Guides",
     url: "https://www.gatsbyjs.com/docs/reference/",
     description:
       "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
+    color: "#BC027F"
   },
   {
     text: "Conceptual Guides",
     url: "https://www.gatsbyjs.com/docs/conceptual/",
     description:
       "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
+    color: "#0D96F2"
   },
   {
     text: "Plugin Library",
     url: "https://www.gatsbyjs.com/plugins",
     description:
       "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
+    color: "#8EB814"
   },
   {
     text: "Build and Host",
@@ -119,17 +120,115 @@ const links = [
     badge: true,
     description:
       "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+    color: "#663399"
+  }
+];
+
+const QuestionComponent = () => {
+  const [answer, setAnswer] = useState("");
+  const [maxattempts, setmaxattempts] = useState(false);
+  const [priceState, setPriceState] = useState(false);
+  const winningMsg = "Congratulations, click to redeem your prize :)";
+  const [attempts, setAttempts] = useState(0);
+  const [showPrize, setShowPrize] = useState(false);
+
+  const question = "What franchise does the melody belong to?";
+  const wrongAnswerPhrases = [
+    "",
+    "Come on",
+    "Try again",
+    "mmhhhh...",
+    "you said the music was intoxicating",
+    "its fast and blue",
+    "it wears gloves",
+    "it has spikes on its back",
+    "really?!? watch the video again:"
+  ];
+
+  const handleAnswerChange = (e) => {
+    setAnswer(e.target.value);
+  };
+
+  const handleCheckAnswer = () => {
+    const correctAnswer = "sonic";
+    if (answer != correctAnswer && attempts != 8) {
+      setAttempts(attempts + 1);
+      if (attempts > 8) {
+        setmaxattempts(true);
+      }
+      console.log("attempts: ", attempts);
+    }
+
+    if (answer.toLowerCase() === correctAnswer.toLowerCase()) {
+      setPriceState(true);
+    }
+  };
+
+  const handleRedeem = () => {
+    setShowPrize(true);
+  };
+
+  const handleToVideo = () => {
+    window.location.assign("https://youtu.be/OMdSDAp4Zu4");
+  };
+
+  return (
+    <div>
+      {!priceState ? (
+        <div>
+          <h2>{question}</h2>
+          <input
+            type="text"
+            value={answer}
+            onChange={handleAnswerChange}
+            placeholder="Type your answer here"
+          />
+          <button onClick={handleCheckAnswer}>Check Answer</button>
+        </div>
+      ) : null}
+
+      <div></div>
+      <p>{attempts == 0 || priceState ? null : wrongAnswerPhrases[attempts]}</p>
+      {wrongAnswerPhrases[attempts] == "really?!? watch the video again:" ? (
+        <p>
+          <p onClick={handleToVideo}>to video</p>
+        </p>
+      ) : null}
+      <div>{priceState ? winningMsg : null}</div>
+      {priceState ? <button onClick={handleRedeem}>Redeem</button> : null}
+      <div>
+        {showPrize ? (
+          <div>
+            <h1>YOU'VE EARNED A TICKET FOR</h1>
+            {/* <Image
+              src="/deich.jpeg"
+              alt="Vercel Logo"
+              className="center"
+              width={700}
+              height={450}
+              priority
+            /> */}
+            <h1>
+              SAVE THE DATE: 24.08.2024 - KIDS IN MEINEM ALTER Tour 2024 -
+              Parkbühne Wuhlheide, Berlin
+            </h1>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+};
 
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
+      <QuestionComponent />
       <h1 style={headingStyles}>
         Congratulations
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
+        <span style={headingAccentStyles}>
+          — you just made a Gatsby site! 🎉🎉🎉
+        </span>
       </h1>
       <p style={paragraphStyles}>
         Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
@@ -144,7 +243,7 @@ const IndexPage = () => {
             {docLink.text}
           </a>
         </li>
-        {links.map(link => (
+        {links.map((link) => (
           <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
             <span>
               <a
@@ -168,9 +267,9 @@ const IndexPage = () => {
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
       />
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Home Page</title>;
